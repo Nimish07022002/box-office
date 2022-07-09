95f598d11f5a8591e08effb6227903faa7d7dd47
NIMISH SURI <nimishsuri5@gmail.com>
2022-07-09 23:05:30 +0530

    upto stared shows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,49 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
 
 const Home = () => {
-  const [input, setInput] = useState(' ');
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
+  const isShowsSearch = searchOption === 'shows';
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
 
   const onInputChange = ev => {
-    // eslint-disable-next-line
-    // console.log(ev)
     setInput(ev.target.value);
   };
 
-
-  const onSearch = () => {
-    // https://api.tvmaze.com/search/shows?q=girls
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-      // eslint-disable-next-line
-        console.log(result)
-      });
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
   };
 
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
 
-  const onKeyDown = ev => {
-    if (ev.keyCode === 13) onSearch();
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -66,7 +85,6 @@ const Home = () => {
       </button>
       {renderResults()}
     </MainPageLayout>
-
   );
 };
 
